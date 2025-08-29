1d713c04b3e3b7c7942d15cef5472c7eaefae081
Suneater29 <arsalan8974@gmail.com>
2025-08-29 21:27:54 +0530

    August 29 '25

--- Array/slargestNoSorting.cpp
+++ Array/slargestNoSorting.cpp
@@ -1,54 +1,54 @@
-//BRUTE FORCE METHOD 
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    vector<int>arr(n);
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    sort(arr.begin(),arr.end());
-    int second;
-    int largest=arr[n-1];
-    for(int i=n-2;i>=0;i--){
-        if(arr[i]!=largest){
-            second=arr[i];
-            break;
-        }
-    }
-    cout<<second;
-    return 0;
-}
-//TC = O(n+nlogn)
+// //BRUTE FORCE METHOD 
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     vector<int>arr(n);
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     sort(arr.begin(),arr.end());
+//     int second;
+//     int largest=arr[n-1];
+//     for(int i=n-2;i>=0;i--){
+//         if(arr[i]!=largest){
+//             second=arr[i];
+//             break;
+//         }
+//     }
+//     cout<<second;
+//     return 0;
+// }
+// //TC = O(n+nlogn)
 
 
 
-//BETTER SOLUTION
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int largest=arr[0];
-    for(int i=0;i<n;i++){
-        if(arr[i]>largest){
-            largest=arr[i];
-        }
-    }
-    int second=-1;
-    for(int i=0;i<n;i++){
-        if(arr[i]>second && arr[i]!=largest){
-            second=arr[i];
-        }
-    }
-    cout<<second;
-    return 0;
-}
+// //BETTER SOLUTION
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int largest=arr[0];
+//     for(int i=0;i<n;i++){
+//         if(arr[i]>largest){
+//             largest=arr[i];
+//         }
+//     }
+//     int second=-1;
+//     for(int i=0;i<n;i++){
+//         if(arr[i]>second && arr[i]!=largest){
+//             second=arr[i];
+//         }
+//     }
+//     cout<<second;
+//     return 0;
+// }
 //TC = o(n)+o(n) = o(2n)
 
 
