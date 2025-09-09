50a782f8d82e8af8c246d92dbd2ca0c7dc89bc5b
Suneater29 <arsalan8974@gmail.com>
2025-09-09 23:27:34 +0530

    September 9 '25

---
+++ Array/Medium/rearrangeArrayElements.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>rearrange(vector<int>&arr,int n){
+    
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int>res=rearrange(arr,n);
+    return 0;
+}
