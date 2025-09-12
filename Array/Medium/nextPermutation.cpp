4c307bb57448e12e581b59ad1ce98a4067c1960c
Suneater29 <arsalan8974@gmail.com>
2025-09-12 21:54:41 +0530

    September 12 '25

---
+++ Array/Medium/nextPermutation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int nxtPermutation(vector<int>&arr,int n){
+    retu
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int res=nxtPermutation(arr,n);
+    return 0;
+}
