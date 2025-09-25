35948759167e6e07e23647cbb8866d9685fe9f12
Suneater29 <arsalan8974@gmail.com>
2025-09-25 19:03:40 +0530

    September 25 '25

--- Array/Hard/3sumOptimized.cpp
+++ Array/Hard/3sumOptimized.cpp
@@ -1,24 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 vector<vector<int>>sum(vector<int>&arr,int n){
-    set<vector<int>>st;
-    int left=0;
-    int right=n-1;
-    while(left<right){
-        int sum=arr[left]+arr[left+1];
-        if(sum = -(arr[right])){
-            vector<int>triplet={arr[left],arr[left+1],arr[right]};
-            sort(triplet.begin(),triplet.end());
-            st.insert(triplet);
-        }
-        else if(sum<arr[right]){
-            left++;
-        }
-        else{
-            right--;
+    vector<vector<int>>ans;
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<n;i++){
+        if(i>0 && arr[i]==arr[i-1]) continue;
+        int j=i+1;
+        int k=n-1;
+        while(j<k){
+            int sum=arr[i]+arr[j]+arr[k];
+            if(sum<0){
+                j++;
+            }
+            else if(sum>0){
+                k--;
+            }
+            else{
+                ans.push_back({arr[i],arr[j],arr[k]});
+                j++;
+                k--;
+                while(j<k && arr[j]==arr[j-1]) j++;
+                while(j<k && arr[k]==arr[k+1]) k--;
+            }
         }
     }
-    return vector<vector<int>>(st.begin(),st.end());
+    return ans;
 }
 int main(){
     int n;
