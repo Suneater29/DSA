dbd245d4c25f79cbaeefbae96c451315b433a4e6
Suneater29 <arsalan8974@gmail.com>
2026-06-23 18:52:00 +0530

    June 23 '26

---
+++ LeetCode/1189-maximumBalloons.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxballoon(string &s){
+    vector<int>mpp(26,0);
+    for(char i:s){
+        mpp[i-'a']++;
+    }
+    return min({mpp['b'-'a'],mpp['a'-'a'],mpp['l'-'a']/2,mpp['o'-'a']/2,mpp['n'-'a']});
+}
+int main(){
+    string s;
+    cin>>s;
+    int ans=maxballoon(s);
+    cout<<ans;
+
+    return 0;
+}
